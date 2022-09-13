fdc4ced08e412f6d19004713dfdd277ede9c1de1
taekgyu <taekgyu0602@gmail.com>
2022-09-13 17:01:43 +0900

    Post수정 테스트 코드 작성

--- src/test/java/com/example/sns/controller/PostControllerTest.java
+++ src/test/java/com/example/sns/controller/PostControllerTest.java
@@ -1,7 +1,10 @@
 package com.example.sns.controller;
 
 import com.example.sns.controller.request.PostCreateRequest;
+import com.example.sns.controller.request.PostModifyRequest;
 import com.example.sns.controller.request.UserJoinRequest;
+import com.example.sns.exception.ErrorCode;
+import com.example.sns.exception.SnsApplicationException;
 import com.example.sns.service.PostService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -14,7 +17,11 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -57,5 +64,69 @@ public class PostControllerTest {
                         .content(objectMapper.writeValueAsBytes(new PostCreateRequest(title, body)))
                 ).andDo(print())
                 .andExpect(status().isUnauthorized());   //.
+    }
+
+    @Test
+    @WithMockUser   // 인증된 상태로 테스트를 진행
+    void 포스트수정() throws Exception{
+        String title = "title";
+        String body = "body";
+
+        mockMvc.perform(put("/api/v1/posts/1")  // 해당 url로 post 요청한다.
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // todo : add request body
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body)))
+                ).andDo(print())
+                .andExpect(status().isOk());   // status가 정상으로 되기를 기대한다.
+    }
+
+    @Test
+    @WithAnonymousUser  // 익명의 유저
+    void 포스트수정시_로그인하지않은경우() throws Exception{
+        String title = "title";
+        String body = "body";
+
+        mockMvc.perform(put("/api/v1/posts/1")  // 해당 url로 post 요청한다.
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // todo : add request body
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body)))
+                ).andDo(print())
+                .andExpect(status().isOk());   // status가 정상으로 되기를 기대한다.
+    }
+
+    @Test
+    @WithMockUser   // 인증된 상태로 테스트를 진행
+    void 포스트수정시_본인이_작성한_글이_아니라면_에러발생() throws Exception{
+        String title = "title";
+        String body = "body";
+
+        // mocking
+        doThrow(new SnsApplicationException(ErrorCode.INVALID_PERMISSION)).
+                when(postService).modify(eq(title), eq(body), any(), eq(1));
+
+        mockMvc.perform(put("/api/v1/posts/1")  // 해당 url로 post 요청한다.
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // todo : add request body
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body)))
+                ).andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser   // 인증된 상태로 테스트를 진행
+    void 포스트수정시_수정하려는_글이_없는경우_에러발생() throws Exception{
+        String title = "title";
+        String body = "body";
+
+        // mocking
+        doThrow(new SnsApplicationException(ErrorCode.POST_NOT_FOUND)).
+                when(postService).modify(eq(title), eq(body), any(), eq(1));
+
+        mockMvc.perform(put("/api/v1/posts/1")  // 해당 url로 post 요청한다.
+                        .contentType(MediaType.APPLICATION_JSON)
+                        // todo : add request body
+                        .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body)))
+                ).andDo(print())
+                .andExpect(status().isNotFound());   // 찾을 수 없기를 기대한다.
     }
 }
---
+++ src/test/java/com/example/sns/fixture/PostEntityFixture.java
@@ -0,0 +1,18 @@
+package com.example.sns.fixture;
+
+import com.example.sns.model.entity.PostEntity;
+import com.example.sns.model.entity.UserEntity;
+
+public class PostEntityFixture {
+    // 가짜 테스트용 UserEntity인 Fixture를 만듦
+    public static PostEntity get(String userName, Integer postId) {
+        UserEntity user = new UserEntity();
+        user.setId(1);
+        user.setUserName(userName);
+
+        PostEntity result = new PostEntity();
+        result.setUser(user);
+        result.setId(postId);
+        return result;
+    }
+}
