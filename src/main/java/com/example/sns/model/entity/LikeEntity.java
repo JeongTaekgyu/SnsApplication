44bfd9b5747535b30ac97f3a771e6646f5d578d1
taekgyu <taekgyu0602@gmail.com>
2022-09-23 20:16:35 +0900

    좋아요 기능 구현

---
+++ src/main/java/com/example/sns/model/entity/LikeEntity.java
@@ -0,0 +1,59 @@
+package com.example.sns.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Entity
+@Table(name = "\"like\"")
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE \"like\" SET deleted_at = NOW() where id =?")  // delete 쿼리가 날라왔을 때 delete된 시간을 처리해줌
+@Where(clause = "deleted_at is NULL") // deleted_At이 NULL 인것만 가져온다.
+public class LikeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")// 외래키가 있는 주인쪽에서 상대방한테 JoinColumn을 건다.
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")// 외래키가 있는 주인쪽에서 상대방한테 JoinColumn을 건다.
+    private PostEntity post;
+
+    @Column(name = "registered_at")
+    private Timestamp registeredAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Column(name = "deleted_at")
+    private Timestamp deletedAt;
+
+    // 매번 엔티티를 만들어 줄때마다 업데이트하고 귀찮으니까 어노테이션으로 자동으로 넣을 수 있도록한다.
+    @PrePersist
+    void registeredAt() {
+        this.registeredAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updateAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    public static LikeEntity of(UserEntity userEntity, PostEntity postEntity) {
+        LikeEntity entity = new LikeEntity();
+        entity.setUser(userEntity);
+        entity.setPost(postEntity);
+
+        return entity;
+    }
+}
--- src/main/java/com/example/sns/service/PostService.java
+++ src/main/java/com/example/sns/service/PostService.java
@@ -3,8 +3,10 @@ package com.example.sns.service;
 import com.example.sns.exception.ErrorCode;
 import com.example.sns.exception.SnsApplicationException;
 import com.example.sns.model.Post;
+import com.example.sns.model.entity.LikeEntity;
 import com.example.sns.model.entity.PostEntity;
 import com.example.sns.model.entity.UserEntity;
+import com.example.sns.repository.LikeEntityRepository;
 import com.example.sns.repository.PostEntityRepository;
 import com.example.sns.repository.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,12 +15,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
 
     private final PostEntityRepository postEntityRepository;
     private final UserEntityRepository userEntityRepository;
+    private final LikeEntityRepository likeEntityRepository;
 
     @Transactional
     public void create(String title, String body, String userName){
@@ -81,7 +86,36 @@ public class PostService {
     }
 
     @Transactional
-    public void like(Integer postId, String usreName) {
+    public void like(Integer postId, String userName) {
+        // post exist
+        PostEntity postEntity = postEntityRepository.findById(postId).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("%s not founded", postId)));
 
+        UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(
+                () -> new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", userName)));
+
+        // check liked -> throw
+        // 이미 해당 게시물에 like 눌렀으면 ALREADY_LIKED 에러 발생시킨다.
+        likeEntityRepository.findByUserAndPost(userEntity, postEntity).ifPresent(it -> {
+            throw new SnsApplicationException(ErrorCode.ALREADY_LIKED, String.format("userName %s already like post %d", userName, postId));
+        });
+
+        // like save
+        likeEntityRepository.save(LikeEntity.of(userEntity, postEntity));
+    }
+
+    @Transactional
+    public int likeCount(Integer postId) {
+        // post exist
+        PostEntity postEntity = postEntityRepository.findById(postId).orElseThrow(() ->
+                new SnsApplicationException(ErrorCode.POST_NOT_FOUND, String.format("%s not founded", postId)));
+
+        // count liked -> throw
+        // 이렇게 가져오면 Post의 list를 전부 가져와야해서 효율적이지 못함
+//        List<LikeEntity> likeEntities = likeEntityRepository.findAllByPost(postEntity);
+//        return likeEntities.size();
+
+        // 해당 포스트의 like count만 가져온다.
+        return likeEntityRepository.countByPost(postEntity);
     }
 }
