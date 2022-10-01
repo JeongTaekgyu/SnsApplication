0787c1876242cc384087ddb67634b185ff633900
taekgyu <taekgyu0602@gmail.com>
2022-10-01 20:42:25 +0900

    알람 기능 테스트코드 수정

--- src/test/java/com/example/sns/controller/UserControllerTest.java
+++ src/test/java/com/example/sns/controller/UserControllerTest.java
@@ -123,7 +123,7 @@ public class UserControllerTest {
     @WithMockUser
     void 알람기능() throws Exception {
         when(userService.alarmList(any(), any())).thenReturn(Page.empty());
-        mockMvc.perform(get("/api/v1/users/alram")
+        mockMvc.perform(get("/api/v1/users/alarm")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
@@ -134,10 +134,10 @@ public class UserControllerTest {
     @WithAnonymousUser
     void 알람리스트_요청시_로그인하지_않은경우() throws Exception {
         when(userService.alarmList(any(), any())).thenReturn(Page.empty());
-        mockMvc.perform(get("/api/v1/users/alram")
+        mockMvc.perform(get("/api/v1/users/alarm")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isUnauthorized());
 
     }
 
