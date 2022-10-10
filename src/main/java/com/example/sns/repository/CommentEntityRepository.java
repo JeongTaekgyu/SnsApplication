ebc9e664a53018c7f072e6bef87cbb42ef7b18a9
taekgyu <taekgyu0602@gmail.com>
2022-10-10 16:33:14 +0900

    삭제시 db에서 데이터를 불러오지 않고 바로 삭제 (deleteAllByPost를 수정)

--- src/main/java/com/example/sns/repository/CommentEntityRepository.java
+++ src/main/java/com/example/sns/repository/CommentEntityRepository.java
@@ -5,10 +5,22 @@ import com.example.sns.model.entity.PostEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CommentEntityRepository extends JpaRepository<CommentEntity, Integer> {
 
     Page<CommentEntity> findAllByPost(PostEntity post, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE CommentEntity entity SET deleted_at = NOW() where entity.post = :post")
+    void deleteAllByPost(@Param("post") PostEntity postEntity);
+    // 영속성을 관리하기 위해서는 db에서 데이터를 불러오는데
+    // 삭제를 하기 위해서 db에서 데이터를 불러오게 하지 않고 삭제 쿼리만 날리게 한다.
+    // 즉, 삭제하기 위해 데이터를 가져오지 않고 삭제 쿼리만 날린다.
 }
--- src/main/java/com/example/sns/repository/LikeEntityRepository.java
+++ src/main/java/com/example/sns/repository/LikeEntityRepository.java
@@ -4,9 +4,11 @@ import com.example.sns.model.entity.LikeEntity;
 import com.example.sns.model.entity.PostEntity;
 import com.example.sns.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,5 +25,9 @@ public interface LikeEntityRepository extends JpaRepository<LikeEntity, Integer>
 
     List<LikeEntity> findAllByPost(PostEntity post);
 
-
+    @Transactional
+    @Modifying
+    @Query("UPDATE LikeEntity entity SET deleted_at = NOW() where entity.post = :post")
+    void deleteAllByPost(@Param("post") PostEntity postEntity);
+    // 삭제하기 위해 데이터를 가져오지 않고 삭제 쿼리만 날린다.
 }
